9126d3e2e4406c68300896b28d987cbd0b984c03
BERAT YETİŞ <127851941+JETIMAM5@users.noreply.github.com>
2023-12-19 18:08:45 +0300

    Add files via upload

---
+++ NestedForLoop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+      int i,j;
+     for(i=1;i<=4;i++)
+        {
+            for(j=1;j<=3;j++)
+                {
+                printf("Hello C\n");
+                }
+        printf("---------\n");
+        }
+    return 0;
+}
