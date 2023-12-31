4735cf7c14a04dd03b7344c1e5dd43381b3cb2c7
BERAT YETİŞ <127851941+JETIMAM5@users.noreply.github.com>
2023-12-31 19:39:06 +0300

    Add files via upload

---
+++ SmallIntroForArrays.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Introduction for Arrays
+    /*
+    int notes[]={7,19,24,3,37};
+    notes[0]=23;
+    for(int i=0 ; i<5;i++)
+        {
+        printf("%d\n",notes[i]);
+        }
+    */
+
+
+     //Finding the average of 3 numbers via arrays
+      /*
+
+      double average =0,numbers[3];
+      printf("Please Enter 3 Numbers\n");
+      scanf("%lf%lf%lf",&numbers[0],&numbers[1],&numbers[2]);
+      average = (numbers[0]+numbers[1]+numbers[2])/3;
+      printf("Average of your numbers:\n%.3lf",average);
+
+      */
+
+
+      //Square a number with arrays
+      /*
+      int i=0, mySquare[5];
+      for(i=0;i<5;i++)
+        {
+        mySquare[i]=i*i;
+        printf("mySquare[%d] is %d\n",i,mySquare[i]);
+        }
+       */
+
+
+
+
+    return 0;
+}
