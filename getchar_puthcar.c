3a64754e8b8f5f6196f51d8ecbcad9c2fb0b788a
BERAT YETİŞ <127851941+JETIMAM5@users.noreply.github.com>
2023-12-31 17:35:22 +0300

    Add files via upload

---
+++ getchar_puthcar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*char myKey;
+    printf("Push a button:\n");
+    myKey= getchar();
+    printf("You pushed %c button\n",myKey);
+    printf("ASCII code is %d\n",myKey);
+    */
+    for(int i=65;i<=80;i++)
+        {
+        putchar(i);
+        printf(" ");
+        }
+
+    return 0;
+}
