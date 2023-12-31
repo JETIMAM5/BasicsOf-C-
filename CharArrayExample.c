037754cdfcf24d4fa576a3bf5863877b198ba8fa
BERAT YETİŞ <127851941+JETIMAM5@users.noreply.github.com>
2023-12-31 20:50:10 +0300

    Add files via upload

---
+++ CharArrayExample.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /*
+    char name[35];
+    printf("Enter your name please:\n");
+    //Common Input Keyword . But it's not a good choce for multiple words : scanf("%s",&name);
+    gets(name);
+    printf("\nYour Name is:%s\n",name);
+    */
+
+
+    return 0;
+}
